044ce648e6ae4e79a65a8529a04eede771e6c1ae
Aemitrius <67407608+Aemitrius@users.noreply.github.com>
2021-10-29 22:34:57 +0530

    Add files via upload

--- aclub/settings.py
+++ aclub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't&=eb=%y!h_w-nd#jzf**x7r6i3@*k7xapx5a5zhqy-mscsq1x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["simpleforsocial.herokuapp.com",'127.0.0.1:8000']
+ALLOWED_HOSTS = ["aclub.herokuapp.com",'127.0.0.1']
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'groups.apps.GroupsConfig',
     'posts.apps.PostsConfig',
-    'misaka',
 ]
 
 MIDDLEWARE = [
